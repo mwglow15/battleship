eb2cb8b1eeca9a4221b49d9b40c6e2812e234962
Matt Glowacki <mattwglowacki@gmail.com>
2023-06-22 09:13:44 -0400

    Create ship constructor and assoc. functions

--- src/ship.js
+++ src/ship.js
@@ -1,3 +1,18 @@
 module.exports = class Ship {
+  constructor(length) {
+    this.length = length
+    this.hits = 0
+  }
 
+  getLength() {
+    return this.length
+  }
+
+  hit() {
+    this.hits += 1
+  }
+
+  getHits() {
+    return this.hits
+  }
 }
--- src/ship.test.js
+++ src/ship.test.js
@@ -1,1 +1,21 @@
 const Ship = require('./ship')
+
+test('getLength', () => {
+  const ship = new Ship(3)
+
+  expect(ship.getLength()).toBe(3)
+})
+
+describe('test hits functions', () => {
+  const ship = new Ship(3)
+
+  test('get initial hits', () => {
+    expect(ship.getHits()).toBe(0)
+  })
+
+  test('increment hits when hit() is called', () => {
+    ship.hit()
+
+    expect(ship.getHits()).toBe(1)
+  })
+})
