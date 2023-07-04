8646366ffe2b91576d1561325bbcbc1c0470d7e1
Matt Glowacki <mattwglowacki@gmail.com>
2023-07-04 09:40:49 -0400

    Add computer moves function

--- src/player.js
+++ src/player.js
@@ -3,7 +3,30 @@ module.exports = class Player {
     this.name = name
   }
 
+  setGameboard(board) {
+    this.gameboard = board
+  }
+
   sendAttack(enemyBoard, x, y) {
     enemyBoard.receiveAttack(x, y)
+  }
+
+  computerAttack(enemyBoard) {
+    let invalidMove = true
+
+    while(invalidMove) {
+      const x = Math.floor(Math.random()) * 10
+      const y = Math.floor(Math.random()) * 10
+
+      const target = enemyBoard.getPosition(x, y)
+
+      if (target) {
+        invalidMove = true
+      } else {
+        invalidMove = false
+        this.sendAttack(enemyBoard, x, y)
+        return { response: true, x: x, y: y, }
+      }
+    }
   }
 }
--- src/player.test.js
+++ src/player.test.js
@@ -1,5 +1,6 @@
 const Player = require('./player')
 const Gameboard = require('./gameboard')
+
 describe('test player class', () => {
   const player = new Player()
   const enemyBoard = new Gameboard()
@@ -9,6 +10,16 @@ describe('test player class', () => {
     player.sendAttack(enemyBoard, 3, 3)
   
     expect(enemyBoard.getPosition(3,3)).toBe('miss')
+  })
+
+  describe('computer can send attack', () => {
+    // test('detects invalid attacks', () => {
+    //   expect(player.computerAttack(enemyBoard)).toBeFalsy()
+    // })
+
+    test('allows valid attacks', () => {
+      expect(player.computerAttack(enemyBoard).response).toBeTruthy()
+    })
   })
 
 })
